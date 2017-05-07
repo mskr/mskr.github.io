3acdcf1e66701bd8f876726086f2db4be0abf861
Marius <marius.kircher@uni-ulm.de>
2017-05-08 01:31:58 +0200

    replace semicolon with comma

--- datavis/01-movies.js
+++ datavis/01-movies.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+    console.log("document ready");
+    var data = [];
+    d3.csv("http://mskr.github.io/datavis/imdb_data_set.csv", function(dat) {
+    	console.log(dat);
+    });
 });
